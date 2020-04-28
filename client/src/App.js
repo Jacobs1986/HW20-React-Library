f478314a96b7f0854c34c0223210c53500588dca
Jacobs1986 <sean.jacobson86@gmail.com>
2020-04-28 15:26:08 -0600

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <>
+            <h1>This is a test.</h1>
+        </>
+    )
+}
+
+export default App;
