117e6c1260422b179a81399c5fc93dec6370e536
Jacobs1986 <sean.jacobson86@gmail.com>
2020-04-28 13:06:12 -0600

    Had to recreate
    Building the app from scratch didn't work, so used the mern example.

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Serve up static assets
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 app.listen(PORT, () => {
-    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
