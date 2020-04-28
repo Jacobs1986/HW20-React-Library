eb15c07a600eda0ac1dfff5e18f606ec501667c9
Jacobs1986 <sean.jacobson86@gmail.com>
2020-04-28 16:21:51 -0600

    Moved boostrap into other package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Books from "./components/pages/BookList";
 
 function App() {
     return (
         <Router>
             <div>
                 <Navbar />
+                <Route exact path="/" component={Books} />
             </div>
         </Router>
     )
---
+++ client/src/components/pages/BookList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Books() {
+    return (
+        <Container>
+            <Row>
+                <h1>Book List</h1>
+            </Row>
+        </Container>
+    )
+}
+
+export default Books;
