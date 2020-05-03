99fd377e69b863e1fe6b76ba90ccb7c6ccf28964
Sean Jacobson <sean.jacobson86@gmail.com>
2020-05-03 17:12:57 -0600

    Update booklist.js
    Started adding some styling and instructions to the page.

--- client/src/components/booklist.js
+++ client/src/components/booklist.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
+// styling
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 class BookList extends Component {
 
     render() {
         return (
-            <div>Hello this is linked!</div> 
+            <Container>
+                <Row>
+                    <h1>Book List</h1>
+                </Row>
+                <Row>
+                    <p>Below you can view the books that you've added to your book list.</p>
+                </Row>
+            </Container>
         )
     }
 }
