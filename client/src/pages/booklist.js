eeaa34054f383e1dc6128f5c6205bb001a6f73e8
Sean Jacobson <sean.jacobson86@gmail.com>
2020-05-03 19:08:58 -0600

    Update booklist.js
    Called to the API and had the database render to the page.

--- client/src/pages/booklist.js
+++ client/src/pages/booklist.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import API from "../utils/API";
 
 // styling
 import Container from 'react-bootstrap/Container';
@@ -6,6 +7,21 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 class BookList extends Component {
+    state = {
+        books: []
+    }
+
+    componentDidMount() {
+        this.loadBooks();
+    }
+
+    loadBooks = () => {
+        API.getBooks()
+            .then(res => {
+                console.log(res.data);
+                this.setState({ books: res.data })
+            });
+    }
 
     render() {
         return (
@@ -16,6 +32,21 @@ class BookList extends Component {
                 <Row>
                     <p>Below you can view the books that you've added to your book list.</p>
                 </Row>
+                {this.state.books.map(books => {
+                    return (
+                        <Row key={books._id}>
+                            <Col md={3}>
+                                <img src={books.image} alt="book cover" />
+                            </Col>
+                            <Col md={9}>
+                                <h4>{books.title}</h4>
+                                <h4>{books.authors}</h4>
+                                <h4>{books.synopsis}</h4>
+                                <a href={books.link}>Click here for more info</a>
+                            </Col>
+                        </Row>
+                    )
+                })}
             </Container>
         )
     }
