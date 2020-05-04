0d2e5fe0acb7560b4ae39f69cd5959e8534f3966
Sean Jacobson <sean.jacobson86@gmail.com>
2020-05-03 18:33:08 -0600

    Created API route

---
+++ routes/api/books.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/bookController")
+
+// Matches with "/api/books"
+router.route("/")
+    .get(booksController.findAll);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("path");
+const routes = require("./routes");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,15 +12,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Add routes
+app.use(routes);
 
 // Connect to the DB
 mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/googlebooks")
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
