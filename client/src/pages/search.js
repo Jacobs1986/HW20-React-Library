233be89066058ad122b4b4f59d96523716643917
Sean Jacobson <sean.jacobson86@gmail.com>
2020-05-04 18:16:35 -0600

    Update search.js

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -2,18 +2,39 @@ import React, { Component } from 'react';
 import API from "../utils/API";
 
 class Search extends Component {
+    state = {
+        searchTitle: "",
+        results: []
+    }
 
-    componentDidMount() {
+    handleChange = event => {
+        this.setState({ searchTitle: event.target.value})
+    };
+
+    searchBook = () => {
         API.searchBook()
         .then(res => {
             console.log(res.data);
         })
     }
 
+    handleButtonClick = event => {
+        event.preventDefault()
+        console.log("you clicked the button")
+        this.searchBook()
+    }
+
     render() {
         return (
             <div>
                 <h1>You are in the search page.</h1>
+                <input 
+                    type="text"
+                    placeholder="Enter title"
+                    value={this.state.searchTitle}
+                    onChange={this.handleChange}
+                />
+                <button onClick={this.handleButtonClick}>Search</button>
             </div>
         )
     }
