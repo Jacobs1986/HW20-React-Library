49d7290bc4c71d4d119bd3dabc37bbb6c3ff7bb2
Sean Jacobson <sean.jacobson86@gmail.com>
2020-05-07 16:39:03 -0600

    Update API.js
    Changed url so that it can accept any searched tital

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const key = "AIzaSyDdRSRCb1Zq_xGMTx360hlCthDqbQZnxwg"
-const url = `https://www.googleapis.com/books/v1/volumes?q=intitle:the+flame+inauthor:cohen&key=${key}`
+const search = title => `https://www.googleapis.com/books/v1/volumes?q=intitle:${title}&key=${key}`
 
 export default {
     // Get all books
     getBooks: function() {
         return axios.get("/api/books");
     },
-    searchBook: function() {
-        return axios.get(url);
+    searchBook: function(title) {
+        return axios.get(search(title));
     }
 }
