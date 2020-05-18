257281537566d630c491a8743dae9d678bff109d
Sean Jacobson <sean.jacobson86@gmail.com>
2020-05-18 11:35:38 -0600

    Create Layout.js

---
+++ client/src/components/Layout.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+
+function Layout(props) {
+    return (
+        <Row id={props.id}>
+            <Col md={3}>
+                <Image src={props.image} fluid/>
+            </Col>
+            <Col md={7}>
+                <h4>Title: {props.title}</h4>
+                <h4>Author: {props.author}</h4>
+                <h6>Summary: {props.summary}</h6>
+                <a href={props.previewLink}>Click here to see more!</a>
+            </Col>
+            <Col md={2}>
+                <button onClick={props.save} id={props.buttonId}>Save Book</button>
+            </Col>
+        </Row>
+    )
+}
+
+export default Layout;
