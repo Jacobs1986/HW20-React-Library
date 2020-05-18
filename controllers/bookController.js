e0f4c271626f0fea3fde0f9f0d96841505aade36
Sean Jacobson <sean.jacobson86@gmail.com>
2020-05-18 11:53:40 -0600

    Update bookController.js
    Added remove route

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -12,5 +12,12 @@ module.exports = {
         .create(req.body)
         .then(dbBook => res.json(dbBook))
         .catch(err => res.status(422).json(err))
-    }
+    },
+    remove: function(req, res) {
+        db.Book
+          .findById({ _id: req.params.id })
+          .then(dbModel => dbModel.remove())
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      }
 }
