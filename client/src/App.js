07952248994c7c8de82aeb4c91da3897510eda4e
Sean Jacobson <sean.jacobson86@gmail.com>
2020-05-18 12:26:32 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import BookList from "./pages/booklist"
 import Search from "./pages/search"
+import Navbar from "./components/Navbar"
 
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.css'
@@ -9,6 +10,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 function App() {
   return (
     <Router>
+      <Navbar />
       <Route exact path="/" component={BookList} />
       <Route exact path="/search" component={Search} />
     </Router>
